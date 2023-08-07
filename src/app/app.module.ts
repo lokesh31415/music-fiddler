469202cf6958d5f83020cbcade612fdb6084659c
loki-dslkhs <storageforprojectfiles@gmail.com>
2023-08-07 22:01:03 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PagesModule } from './pages/pages.module';
+import { AppMaterialModule } from './app-material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, PagesModule],
+  imports: [BrowserModule, CoreModule, PagesModule, AppMaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
